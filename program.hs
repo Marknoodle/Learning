2e95cc606d2546616ac32fb4a61940288b7466c6
Mark Holcomb <markh@localhost.localdomain>
2019-06-08 17:47:08 -0400

    Added code for problem 3.2

--- program.hs
+++ program.hs
@@ -11,3 +11,9 @@ qthree x = if even x
 doubleDouble x = (\x -> x*2) x * 2
 
 overwrite x = (\x -> (\x -> (\x -> x) 4) 3) 2
+
+sumSquareOrSquareSum x y = (\sumSquare squareSum ->
+                             if sumSquare > squareSum
+                             then sumSquare
+                             else squareSum) (x^2 + y^2) ((x + y)^2)
+
